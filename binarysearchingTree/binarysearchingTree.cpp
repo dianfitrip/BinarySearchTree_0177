81d033b4b4bb29be759d58ab1719fa0ade3d7330
dianfitrip <dianfitri683@gmail.com>
2024-06-13 19:02:52 +0700

    menambahkan(void insert)

--- binarysearchingTree/binarysearchingTree.cpp
+++ binarysearchingTree/binarysearchingTree.cpp
@@ -26,3 +26,31 @@ public:
     {
         ROOT = nullptr; // Initializin ROOT to null
     }
+
+    void insert(string element) // insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, nullptr, nullptr); // allocate memory for the new node
+        newNode->info = element; // asign value to the data field of the new node
+        newNode->leftchild = nullptr; // make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; // make the right child of the new node point to NULL
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // if the parent is NULL (tree is empty)
+        {
+            ROOT = newNode; // mark the new node as root
+            return; // exit
+        }
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the
+        {
+            parent->leftchild = newNode; // make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // if the value in the data field of the new node is greater than that
+        {
+            parent->rightchild = newNode; // make the right child of the parent point to the new node
+        }
+    }
+
+    
