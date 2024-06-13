96a421db06310c901816f5eb2747f483759ae9b9
dianfitrip <dianfitri683@gmail.com>
2024-06-13 19:10:01 +0700

    5. exit. dan menampilkan menu enteryour choice

--- binarysearchingTree/binarysearchingTree.cpp
+++ binarysearchingTree/binarysearchingTree.cpp
@@ -121,4 +121,7 @@ int main()
         cout << "2. Perform inorder traversal" << endl;
         cout << "3. Perfomr preorder traversal" << endl;
         cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
         
